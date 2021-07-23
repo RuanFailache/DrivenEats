483b21522f4e8c8e0c0f249d2f3ff602441e1bad
RuanFailache <ruanfailache@outlook.com>
2021-07-23 13:31:04 -0300

    Whatsapp implementation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const dishes = document.querySelectorAll(".dish");
 const drinks = document.querySelectorAll(".drink");
 const desserts = document.querySelectorAll(".dessert");
 
-const btn = document.querySelector("button");
+const btn = document.querySelector(".btn");
 
 dishes.forEach((dish) => {
   const checkDish = () => {
@@ -57,16 +57,48 @@ const btnEnabler = () => {
     const isDrinkActive = document.querySelectorAll(".drink.active");
     const isDessertActive = document.querySelectorAll(".dessert.active");
 
-    let text = document.querySelector("button > span");
+    let text = document.querySelector(".btn > span");
 
     if (isDishActive.length > 0 && isDrinkActive.length > 0 && isDessertActive.length > 0) {
-        btn.disabled = false;
-        btn.style.backgroundColor = "#32B72F";
-        btn.style.paddingTop = "22px";
-        btn.style.paddingBottom = "22px";
-        btn.style.cursor = "pointer";
-        text.innerHTML = "Fechar pedido"
+      btn.classList.add("active");  
+      btn.disabled = false;
+      text.innerHTML = "Fechar pedido"
     } else {
         btn.disabled = true;
     }
 }
+
+const sendMessageToWhatsapp = () => {
+  
+  btnEnabler();
+
+  const messageMaker = () => {
+
+    const dishActiveName = document.querySelector(".dish.active > .name").innerHTML;
+    const drinkActiveName = document.querySelector(".drink.active > .name").innerHTML;
+    const dessertActiveName = document.querySelector(".dessert.active > .name").innerHTML;
+
+    const dishActivePrice = Number(document.querySelector(".dish.active > .price").innerHTML.substr(3));
+    const drinkActivePrice = Number(document.querySelector(".drink.active > .price").innerHTML.substr(3));
+    const dessertActivePrice = Number(document.querySelector(".dessert.active > .price").innerHTML.substr(3));
+    
+    let totalPrice = dishActivePrice + drinkActivePrice + dessertActivePrice;
+    totalPrice = totalPrice.toFixed(2);
+
+    const message = `\
+      Olá, gostaria de fazer o pedido:\n\
+      *- Prato:* ${dishActiveName}\n\
+      *- Bebida:* ${drinkActiveName}\n\
+      *- Sobremesa:* ${dessertActiveName}\n\
+      *Total:* R$ ${totalPrice}`;
+    return encodeURIComponent(message);;
+  }
+
+  if ( !btn.disabled ) {
+    const message = messageMaker();
+    const phoneNumber = "5591991535878";
+    window.open(`https://wa.me/${phoneNumber}?text=${message}`);
+  }
+}
+
+btn.addEventListener("click", sendMessageToWhatsapp);
